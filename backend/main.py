79936e06d826217189a7a9b4a62bc65b0d4cb1b3
nishan <nishan.paudel1914@gmail.com>
2025-05-04 14:33:23 -0700

    Revert "MQTT added"
    This reverts commit 823b15bc19a8b16179ee5376ae36b3e68eea7e3a.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from mqtt_client import start_mqtt_loop, logger as mqtt_logger
 
+from api.alerts import AlertsAPI
 from api.device import DeviceAPI
 from api.records import RecordsAPI
 from core.database import db
@@ -12,20 +12,9 @@ from core.database import db
 async def lifespan(app: FastAPI):
     # Startup logic
     await db.connect_to_database()
-    print("Starting up...")
-    mqtt_logger.info("Application starting up...")
-    # Start MQTT client loop
-    mqtt_client = start_mqtt_loop()
     yield
     # Shutdown logic
     await db.close_database_connection()
-    print("Shutting down...")
-    mqtt_logger.info("Application shutting down...")
-    # Stop MQTT client loop
-    if mqtt_client:
-        mqtt_client.loop_stop()
-        mqtt_client.disconnect()
-        mqtt_logger.info("MQTT client disconnected on shutdown.")
 
 
 app = FastAPI(
@@ -52,33 +41,11 @@ app.add_middleware(
 # Include routers
 device_api = DeviceAPI()
 record_api = RecordsAPI()
-# gig_api = GigJobAPI()
-# volunteer_api = VolunteerJobAPI()
-# application_api = ApplicationAPI()
-# user_api = UserAPI()
+alerts_api = AlertsAPI()
+
 
 # app.include_router(oauth_router)
 app.include_router(device_api.router, prefix="/api/device", tags=["Device"])
 app.include_router(record_api.router, prefix="/api/record", tags=["Record"])
-# app.include_router(gig_api.router, prefix="/api/gig", tags=["Gigs"])
-# app.include_router(volunteer_api.router, prefix="/api/volunteer", tags=["Volunteers"])
-# app.include_router(application_api.router, prefix="/api/application", tags=["Application"])
-# app.include_router(user_api.router, prefix="/api/user", tags=["User"])
+app.include_router(alerts_api.router, prefix="/api/alert", tags=["Alert"])
 
-@app.on_event("startup")
-def startup_event():
-    global mqtt_client
-    # Start MQTT client loop
-    mqtt_client = start_mqtt_loop()
-
-@app.on_event("shutdown")
-def shutdown_event():
-    if mqtt_client:
-        # Stop MQTT client loop
-        mqtt_client.loop_stop()
-        mqtt_client.disconnect()
-        mqtt_logger.info("MQTT client disconnected on shutdown.")
-
-if __name__ == "__main__":
-    # Note: In production environments, Gunicorn + Uvicorn workers are typically used
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) # Add reload=True for automatic reloading during development
