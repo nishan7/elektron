97f26562419d6960cec22524ad9c830651e059a5
Weiyu Wang <weiyu.wang@sjsu.edu>
2025-05-05 23:01:31 +0800

    Delete frontend directory

--- frontend/src/API.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Create a pre-configured Axios instance
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
-});
-
-
-export default API;
