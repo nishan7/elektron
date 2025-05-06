03e5078fb57874d3bd87b22ca42e8284eba37805
Weiyu Wang <weiyu.wang@sjsu.edu>
2025-05-07 03:23:02 +0800

    Delete backend directory

--- backend/core/sync_database.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from pymongo import MongoClient
-
-mongo_uri = os.getenv("MONGO_URI", "mongodb://root:example@localhost:27017/elektron?authSource=admin")
-
-db = MongoClient(mongo_uri)["elektron"]
-
--- backend/models/base.py
+++
@@ -1,9 +0,0 @@
-from bson import ObjectId
-from pydantic import BaseModel, Field
-from typing import Optional, Annotated, Any, Callable
-from pydantic_mongo import PydanticObjectId
-
-
-# Base to be used by all models
-class Base(BaseModel):
-    id: PydanticObjectId = Field(default_factory=PydanticObjectId, alias="_id")
