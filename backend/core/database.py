1c745e70a636454045a18a47c00da36fd5767d73
nishan <nishan.paudel1914@gmail.com>
2025-05-07 14:38:46 -0700

    Add print statement for mongo_uri in database connection

--- backend/core/database.py
+++ backend/core/database.py
@@ -9,6 +9,7 @@ class Database:
         mongo_uri = os.getenv("MONGO_URI", "mongodb+srv://cmpe-272:cmpe-272@cluster0.crznvzq.mongodb.net/elektron?retryWrites=true&w=majority&appName=Cluster0")
         # mongo_uri = os.getenv("MONGO_URI", "mongodb://root:example@mongo:27017/elektron?authSource=admin")
         self.client = AsyncIOMotorClient(mongo_uri)
+        print(mongo_uri)
         self.db = self.client.get_database()
 
     async def close_database_connection(self):
--- backend/core/sync_database.py
+++ backend/core/sync_database.py
@@ -3,6 +3,5 @@ import os
 from pymongo import MongoClient
 
 mongo_uri = os.getenv("MONGO_URI", "mongodb+srv://cmpe-272:cmpe-272@cluster0.crznvzq.mongodb.net/elektron?retryWrites=true&w=majority&appName=Cluster0")
-
 db = MongoClient(mongo_uri)["elektron"]
 
